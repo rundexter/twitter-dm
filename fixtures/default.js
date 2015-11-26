171fa1a6f7cb5ab2b12594390a016bbff0fed6f1
Alexander Zaretskii <al.zaretskii@gmail.com>
2015-11-26 13:42:13 +0200

    add DEXTER module

--- fixtures/default.js
+++ fixtures/default.js
@@ -63,7 +63,8 @@ module.exports = _.merge({
              */
             input: {
                 //Replace VAR1 with the name of an expected input, and add more inputs as needed.
-                VAR1: 'foo'
+                screen_name: 'noradio',
+                text: 'Text message'
             }
         }
     }
